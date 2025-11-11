74d30fa8689112f70f3ff11e0c32ffba7d840adc
suzuki119 <suzukiyutaro119@gmail.com>
2025-11-11 16:50:39 +0900

    1111

--- 1111/color_scroll/script.js
+++ 1111/color_scroll/script.js
@@ -1,3 +1,5 @@
+//1オブジェクトで色を用意
+//const オブジェクト = キー:プロパティ
 
 //const オブジェクト = キー:プロパティ
 const settingColors =
@@ -18,6 +20,11 @@ console.log(settingColors);
     ')';
     */
 
+
+
+
+//2オブジェクトの色をbodyの背景色にする
+
 document.body.style.backgroundColor =
     'rgb(' +
     settingColors[0].r +
@@ -26,6 +33,32 @@ document.body.style.backgroundColor =
     ',' +
     settingColors[0].b +
     ')';
+
+
+
+/*背景色の設定方法
+'rgb(' + r値 + ',' + g値 + ',' +b値 + ')';
+*/
+
+
+//3 4色制作して、配列にする
+
+
+
+//4スクロールするたびにイベント
+//5documentの縦の長さ取得
+//6 1 / 4進んだら色が変わるようにする
+//7スクロールを4分割
+//8スクロール量を計算
+//9変数scrollableを4分割
+//10関数の定義
+//11関数の呼び出し（実行）の実引数
+
+
+
+
+
+
 
 function changeColor(num) {
     document.body.style.backgroundColor = "rgb(" + settingColors[num].r + "," + settingColors[num].g + "," + settingColors[num].b + ")";
@@ -44,6 +77,12 @@ const scrollY = window.scrollY;//現在の位置参照
 console.log(scrollY);//現在の位置
 
 //スクロールするたびに(（"scroll"）)実行するイベントの追加
+
+
+
+
+
+
 window.addEventListener("scroll", function () {
 
     //画面の高さを参照(windowそのものの高さ)
